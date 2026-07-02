f88561b7b65b76a4bc204f43300853179f8aac3b
Marcio Henrique <marcio@sket-soft.com>
2026-07-02 15:36:57 -0300

    refatoração da configuração swagger em modulo

--- src/Ofichina.Api/Program.cs
+++ src/Ofichina.Api/Program.cs
@@ -1,54 +1,23 @@
-using Microsoft.OpenApi;
+using Ofichina.Api.Modules;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Ofichinna API",
-        Description = "Api Desenvolvida para o Tech Challenge da Pós Tech FIAP",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Contato",
-            Url = new Uri("https://example.com/contact")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Licença de Uso",
-            Url = new Uri("https://example.com/license")
-        }
-
-
-    });
-});
-
 builder.Services.AddAuthorization();
+
+builder.Services.AddSwaggerModule();
 
 var app = builder.Build();
 
-if (builder.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
-
-// Configuracao do Pipeline de Middleware
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerModule();
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
