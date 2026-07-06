01a481e5d57a0f4818918ca056e6b1ee37755bd0
Marcio Henrique <marcio@sket-soft.com>
2026-07-06 15:01:30 -0300

    #14 Neste PR, realizei a configuracao de um projeto bootstrap, para ser o concentrador dos modulos do projeto, dessa forma, a camada de api nunca conhecera a camada de infrastructure/application diretamente

---
+++ Ofichina.Bootstrap/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ofichina.Application.DependencyInjection;
+using Ofichina.Infrastructure.DependencyInjection;
+
+namespace Ofichina.Bootstrap;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBootstrap(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddInfrastructure(configuration);
+
+        return services;
+    }
+}
