4151e9718f2aecaff9779e9ee0f2155450b7699a
Marcio Henrique <marcio@sket-soft.com>
2026-07-06 18:42:25 -0300

    nesse pr fiz corretamente o vinculo do sonarqube com o projeto dotnet atraves do docker

--- src/Ofichina.Application/DependencyInjection/ApplicationModule.cs
+++ src/Ofichina.Application/DependencyInjection/ApplicationModule.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Ofichina.Infrastructure.DependencyInjection;
 
 namespace Ofichina.Application.DependencyInjection;
 
