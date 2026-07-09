15a5086c316d505cc9586e16c08b001b60725eca
Marcio Henrique <marcio@sket-soft.com>
2026-07-09 13:02:03 -0300

    validators de command e request

---
+++ src/Ofichina.Application/Validators/Cliente/VincularPerfilClienteRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ofichina.Application.Validators.Cliente
+{
+    public class VincularPerfilClienteRequestValidator
+    {
+    }
+}
