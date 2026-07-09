725ba8d8da0df0db2ba3855b5899ff1d9660daf7
Marcio Henrique <marcio@sket-soft.com>
2026-07-09 18:18:58 -0300

    Contratos: Ajuste nos arquivos de Enum e alteracao do nome da pasta para perfilclientes

--- src/Ofichina.Contracts/Requests/PerfilCliente/DesvincularPerfilClienteRequest.cs
+++ src/Ofichina.Contracts/Requests/PerfilCliente/DesvincularPerfilClienteRequest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Ofichina.Contracts.Requests.Cliente
+namespace Ofichina.Contracts.Requests.PerfilCliente
 {
     internal class DesvincularPerfilClienteRequest
     {
