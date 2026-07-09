2e7835648c99dd1911edb935d0632f5984fa41c2
Marcio Henrique <marcio@sket-soft.com>
2026-07-09 00:02:06 -0300

    queries

---
+++ src/Ofichina.Application/UseCases/Perfil/Queries/GetPerfilByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using Ofichina.Application.Abstractions;
+using Ofichina.Contracts.Responses.Perfil;
+
+namespace Ofichina.Application.UseCases.Perfil.Queries;
+
+/// <summary>
+/// Query para obter um perfil por ID.
+/// </summary>
+public class GetPerfilByIdQuery : IQuery<PerfilResponse?>
+{
+    public Guid Id { get; set; }
+
+    public GetPerfilByIdQuery(Guid id)
+    {
+        Id = id;
+    }
+}
