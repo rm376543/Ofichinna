332977ec60be62794b244e659405dafb8e0c23a1
Marcio Henrique <marcio@sket-soft.com>
2026-07-09 14:14:07 -0300

    commands e handlers

---
+++ src/Ofichina.Application/UseCases/Autenticacao/Commands/AutenticarCommand.cs
@@ -0,0 +1,17 @@
+﻿using Ofichina.Application.Abstractions;
+using Ofichina.Contracts.Responses;
+using Ofichina.Domain.ValueObjects;
+
+namespace Ofichina.Application.UseCases.Autenticacao.Commands;
+
+public sealed class AutenticarCommand : ICommand<Result<AutenticacaoResponse>>
+{
+    public string Email { get; init; } = string.Empty;
+    public string Senha { get; init; } = string.Empty;
+
+    public AutenticarCommand(string email, string senha)
+    {
+        Email = email;
+        Senha = senha;
+    }
+}
---
+++ src/Ofichina.Application/UseCases/Autenticacao/Commands/CadastrarClienteCommand.cs
@@ -0,0 +1,19 @@
+﻿using Ofichina.Application.Abstractions;
+using Ofichina.Contracts.Responses;
+using Ofichina.Domain.ValueObjects;
+
+namespace Ofichina.Application.UseCases.Autenticacao.Commands;
+
+public sealed class CadastrarClienteCommand : ICommand<Result<AutenticacaoResponse>>
+{
+    public string Nome { get; }
+    public string Email { get; }
+    public string Senha { get; }
+
+    public CadastrarClienteCommand(string nome, string email, string senha)
+    {
+        Nome = nome;
+        Email = email;
+        Senha = senha;
+    }
+}
