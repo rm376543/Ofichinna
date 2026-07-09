8db6a41df52ae8561f5c174f5ebf76140761ad22
Marcio Henrique <marcio@sket-soft.com>
2026-07-09 12:46:55 -0300

    contratos (request/response)

---
+++ src/Ofichina.Contracts/Responses/Cliente/DesvincularPerfilClienteResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Ofichina.Contracts.Responses.Cliente;
+
+public sealed class DesvincularPerfilClienteResponse
+{
+    public Guid UsuarioId { get; set; }
+
+    public Guid PerfilId { get; set; }
+
+    public string Mensagem { get; set; } = "Perfil desvinculado com sucesso.";
+}
