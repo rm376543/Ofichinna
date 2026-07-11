167dc0baebde1a51f22bf1570ab405639746b8b0
Marcio Henrique <marcio@sket-soft.com>
2026-07-11 00:06:44 -0300

    authentication: criacao do middleware de captura de evento nao autenticado automatico, remocao da claim nome na geracao do token jwt e ajuste na regra de validacao

---
+++ src/Ofichina.Authentication/DependencyInjection/AuthorizationResultHandlerModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.Extensions.DependencyInjection;
+using Ofichina.Authentication.Security;
+
+namespace Ofichina.Authentication;
+
+public static class AuthorizationResultHandlerModule
+{
+    public static IServiceCollection AddAuthorizationResultHandlerModule(
+        this IServiceCollection services)
+    {
+        services.AddSingleton<IAuthorizationMiddlewareResultHandler, ApiAuthorizationMiddlewareResultHandler>();
+        return services;
+    }
+}
