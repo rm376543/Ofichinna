fbe9fc11f59d4f14bf5a111d40444f0f10f2941f
Marcio Henrique <marcio@sket-soft.com>
2026-07-12 21:39:45 -0300

    interface : renomeando de perfilCliente para perfil usuario

--- src/Ofichina.Domain/Interfaces/IPerfilUsuarioRepository.cs
+++ src/Ofichina.Domain/Interfaces/IPerfilUsuarioRepository.cs
@@ -2,7 +2,7 @@
 
 namespace Ofichina.Domain.Interfaces;
 
-public interface IClientePerfilRepository : IRepository<UsuarioPerfil>
+public interface IPerfilUsuarioRepository : IRepository<UsuarioPerfil>
 {
     Task<bool> ExisteAsync(Guid clienteId, Guid perfilId);
     Task<UsuarioPerfil?> GetByUsuarioIdPerfilIdAsync(Guid usuarioId, Guid perfilId);
