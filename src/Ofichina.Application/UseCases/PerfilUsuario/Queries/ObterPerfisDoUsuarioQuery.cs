c9c4414abdbd311a54b14a46e03902cb4bbeeb07
Marcio Henrique <marcio@sket-soft.com>
2026-07-12 21:38:22 -0300

    usecase: renomeando perfil cliente para perfil usuario

---
+++ src/Ofichina.Application/UseCases/PerfilUsuario/Queries/ObterPerfisDoUsuarioQuery.cs
@@ -0,0 +1,13 @@
+﻿using Ofichina.Application.Abstractions;
+
+namespace Ofichina.Application.UseCases.PerfilUsuario.Queries;
+
+public sealed class ObterPerfisDoUsuarioQuery : IQuery<IReadOnlyCollection<string>>
+{
+    public Guid UsuarioId { get; }
+
+    public ObterPerfisDoUsuarioQuery(Guid usuarioId)
+    {
+        UsuarioId = usuarioId;
+    }
+}
