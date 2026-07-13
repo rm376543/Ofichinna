8fe6537961c199eaef9e48a8144823f2aa8eec59
Marcio Henrique <marcio@sket-soft.com>
2026-07-12 21:44:09 -0300

    classes de exceptions, domain e business

---
+++ src/Ofichina.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿namespace Ofichina.Domain.Exceptions;
+
+public sealed class DomainException : Exception
+{
+    public DomainException(string message)
+        : base(message)
+    {
+    }
+
+    public DomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
