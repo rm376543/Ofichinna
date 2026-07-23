6642bf5396880562a609cd0aff7644d7ea6bf222
Marcio Henrique <marcio@sket-soft.com>
2026-07-23 17:01:25 -0300

    Criando um DTO DesvincularPerfilPermisao para controller de vinculo de perfil com permissao

---
+++ src/Ofichina.Contracts/Responses/PerfilPermissoes/DesvincularPerfilPermissao.cs
@@ -0,0 +1,10 @@
+﻿using Ofichina.Contracts.Requests;
+
+namespace Ofichina.Contracts.Responses.PerfilPermissoes
+{
+    public class DesvincularPerfilPermissao : BaseRequest
+    {
+        public Guid PerfilId { get; set; }
+        public Guid PermissaoId { get; set; }
+    }
+}
