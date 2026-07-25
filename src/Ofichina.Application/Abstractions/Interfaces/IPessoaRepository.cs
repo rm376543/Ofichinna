7f38c5bd94bd38f9649193036157e6d261027d20
Marcio Henrique <marcio@sket-soft.com>
2026-07-25 17:43:22 -0300

    realizei a correcao de 2 endpoints - listar todos agora retorna dados paginados - alterar nao tem mais id no query - criar nao retorna mais um id no response

--- src/Ofichina.Application/Abstractions/Interfaces/IPessoaRepository.cs
+++ src/Ofichina.Application/Abstractions/Interfaces/IPessoaRepository.cs
@@ -1,10 +1,33 @@
-﻿using Ofichina.Domain.Entities;
+﻿using Ofichina.Contracts;
+using Ofichina.Contracts.Common;
+using Ofichina.Domain.Entities;
 
 namespace Ofichina.Application.Abstractions.Interfaces;
 
 public interface IPessoaRepository : IRepository<Pessoa>
 {
+    /// <summary>
+    /// Obtém uma pessoa pelo ID do usuário associado.
+    /// </summary>
+    /// <param name="usuarioId">ID do usuário associado à pessoa.</param>
+    /// <param name="cancellationToken">Token de cancelamento.</param>
+    /// <returns>A pessoa associada ao ID do usuário, ou null se não encontrada.</returns>
     Task<Pessoa?> GetByUsuarioIdAsync(Guid usuarioId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Obtém uma pessoa pelo ID da pessoa, incluindo os veículos associados.
+    /// </summary>
+    /// <param name="pessoaId">ID da pessoa.</param>
+    /// <param name="cancellationToken">Token de cancelamento.</param>
+    /// <returns>A pessoa associada ao ID informado, incluindo os veículos, ou null se não encontrada.</returns>
     Task<Pessoa?> GetByIdWithVeiculosAsync(Guid pessoaId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Pesquisa todas pessoas e mostra paginadas de acordo com a paginação informada.
+    /// </summary>
+    /// <param name="pagination">Informações de paginação.</param>
+    /// <param name="cancellationToken">Token de cancelamento.</param>
+    /// <returns>Uma resposta paginada contendo as pessoas.</returns>
+    Task<PagedResponse<Pessoa>> GetAllPessoasPaginadasAsync(Pagination pagination, CancellationToken cancellationToken = default);
 }
 
---
+++ src/Ofichina.Application/UseCases/Pessoa/Queries/GetAllPessoasPaginadasQuery.cs
@@ -0,0 +1,20 @@
+using Ofichina.Application.Abstractions;
+using Ofichina.Contracts;
+using Ofichina.Contracts.Common;
+using Ofichina.Contracts.Responses.Pessoa;
+using Ofichina.Domain.Common;
+
+namespace Ofichina.Application.UseCases.Pessoas.Queries;
+
+/// <summary>
+/// Query para listar pessoas.
+/// </summary>
+public sealed class GetAllPessoasPaginadasQuery : IQuery<Result<PagedResponse<PessoaResponse>>>
+{
+    public Pagination Pagination { get; }
+
+    public GetAllPessoasPaginadasQuery(Pagination pagination)
+    {
+        Pagination = pagination;
+    }
+}
