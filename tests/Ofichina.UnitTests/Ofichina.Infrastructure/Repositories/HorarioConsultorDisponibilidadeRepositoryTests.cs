eb7f2e082928b173fe1b5229eb0c4a8bcf6ad7aa
Marcio Henrique <marcio@sket-soft.com>
2026-08-20 10:48:30 -0300

    Infrastructure

--- tests/Ofichina.UnitTests/Ofichina.Infrastructure/Repositories/HorarioConsultorDisponibilidadeRepositoryTests.cs
+++ tests/Ofichina.UnitTests/Ofichina.Infrastructure/Repositories/HorarioConsultorDisponibilidadeRepositoryTests.cs
@@ -4,7 +4,7 @@ using Ofichina.Domain.ValueObjects;
 using Ofichina.Infrastructure.Persistence;
 using Ofichina.Infrastructure.Repositories;
 
-namespace Ofichina.UnitTests.Infrastructure.Repositories;
+namespace Ofichina.UnitTests.Ofichina.Infrastructure.Repositories;
 
 public sealed class HorarioConsultorDisponibilidadeRepositoryTests
 {
